917a774b2e9e7a970fc9f4850dcbdbe4167636e0
zion <130053047+tsitiii@users.noreply.github.com>
2023-12-07 18:33:05 +0300

    update has been procceded

--- week3/SumOf_even_numbers.py
+++ week3/SumOf_even_numbers.py
@@ -1,16 +1,19 @@
 sum=0
 count_3=0
 count_5=0
-for i in range(1,51):
+num=[]
+for i in range(1,50):
     if i%2==0:
         if i%3==0:
             count_3+=1
-            print("Three")
+            num.append("Three")
         elif i%5==0:
             count_5+=1
-            print("Five")
+            num.append("Five")
         else:
          sum+=i
-print(f"sum: {sum}")
+         num.append(i)
+print(num)
+print(f"\nTotal sum of even number except 3 and 5 is: {sum}")
 print(f"Number of times 3 replaced is {count_3}")
 print(f"Number of times 5 replaced is {count_5}")
